0e4adcfacf2b1a47f0cac4c36fe86848a715e334
celestine <okorocelestine@gmail.com>
2017-01-10 16:19:13 +0100

    Add some documentation to the index file for the ClI project

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
+/**
+ *  A function expect an input which is the keyword to search and it returns a list of URL related to the keyword 
+ * @Author okorocelstine@gmail.com (Okoro Celestine)
+ */
 #! /usr/bin/env node
+// use the jsearch module to search
 var js = require('jsearch');
 js.google( escape(process.argv),10,function(response){
     console.log(response) // for Google results 
